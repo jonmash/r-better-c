0450580ad522f49da13aedff9afa8a92315bc273
Jonathan Mash <jmash@sparqsys.com>
2016-02-03 13:51:18 -0800

    Initial commit of first pass

---
+++ main.js
@@ -0,0 +1,23 @@
+javascript:
+(function() {
+  var newcss=" .flexCard{padding: 0;}"+
+    " #accProductsBanner{padding: 0;}"+
+    " .rewardsBanner{display: none;}"+
+    " .accProductDetail .tableBody tr td,"+
+    " .accProductDetail .tableBody tr th { padding: 0; }"+
+    " .table>tbody>tr>td,"+
+    " .table>tbody>tr>th,"+
+    " .table>tfoot>tr>td,"+
+    " .table>tfoot>tr>th,"+
+    " .table>thead>tr>td,"+
+    " .table>thead>tr>th { padding: 0; }";
+  if("\v"=="v") {
+    document.createStyleSheet().cssText=newcss
+  } else {
+    var tag=document.createElement("style");
+    tag.type="text/css";
+    document.getElementsByTagName("head")[0].appendChild(tag);
+    tag[(typeof document.body.style.WebkitAppearance=="string") ? "innerText" : "innerHTML"] = newcss
+  }
+})();
+
